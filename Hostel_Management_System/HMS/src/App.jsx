cf478f7eca493263d315036c1a84033704c0b1af
TheAshish07 <ashsah.cs.eng@gmail.com>
2025-01-26 11:03:07 +0530

    staffdashboard is some done

--- Hostel_Management_System/HMS/src/App.jsx
+++ Hostel_Management_System/HMS/src/App.jsx
@@ -1,11 +1,14 @@
 
-import React from 'react'
 // import App1 from './admindashboard/Component/App1'
-import App2 from './FrontEndComponent/Component/App2'
+import App3 from './staffdashboard/component/App3'
+//import ComplaintsList from './staffdashboard/component/complaintstaff'
+//import StaffDashboard from './staffdashboard/component/dashboard1'
+// import App from './FrontEndComponent/Component/App2'
+
  function App() {
   return (
     <div>
-      <App2/>
+      <App3/>
     </div>
   )
 }
