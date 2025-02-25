102cab43fed730ae1fc7d35e8c75d071269800c6
Ashish sah <136005244+TheAshish07@users.noreply.github.com>
2025-02-25 14:40:16 +0530

    PostgreSQL database connection done successfully

--- Hostel_Management_System/Backend/postgreSQL/postgres.js
+++ Hostel_Management_System/Backend/postgreSQL/postgres.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 
 
-const sequelize = new Sequelize('postgres', 'root', 'root', {
+const sequelize = new Sequelize('postgres', 'postgres', 'postgresql', {
     host: 'localhost',
     dialect: 'postgres'
   });
